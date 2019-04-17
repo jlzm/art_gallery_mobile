c8f198bad4ae61e5c58e8b30c58b8692864b1ecd
jlzm <changhui1118@foxmail.com>
2019-04-17 11:44:33 +0800

    leaveBtn-debug

--- src/global/global.js
+++ src/global/global.js
@@ -1,6 +1,6 @@
 const contentType = 'application/x-www-form-urlencoded; charset=UTF-8';
-const baseurl = 'http://172.16.10.184:8080/HappyMomaArt/';
-// const baseurl = 'http://saas.csgxcf.com:8585/HappyMomaArt/';
+// const baseurl = 'http://172.16.10.184:8080/HappyMomaArt/';
+const baseurl = 'http://saas.csgxcf.com:8585/HappyMomaArt/';
 
 const imgUrl = baseurl + 'getImgUrl?ftype=photo&imgurl=';
 const videoUrl = baseurl + 'getImgUrl?ftype=view&imgurl=';
