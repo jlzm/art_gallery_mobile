028aea3264bf2234df3d548ef00989a529f769ff
jlzm <changhui1118@foxmail.com>
2019-06-05 15:09:57 +0800

    button样式改变

--- src/global/global.js
+++ src/global/global.js
@@ -1,6 +1,6 @@
 const contentType = 'application/x-www-form-urlencoded; charset=UTF-8';
-const baseurl = 'http://172.16.10.201:8080/HappyMomaArt/';
-// const baseurl = 'http://saas.csgxcf.com:9501/HappyMomaArt/';
+// const baseurl = 'http://172.16.10.201:8080/HappyMomaArt/';
+const baseurl = 'http://saas.csgxcf.com:9501/HappyMomaArt/';
 
 const imgUrl = baseurl + 'getImgUrl?ftype=photo&imgurl=';
 // const imgUrl = baseurl + 'showPicture?ftype=photo&imgurl=';
